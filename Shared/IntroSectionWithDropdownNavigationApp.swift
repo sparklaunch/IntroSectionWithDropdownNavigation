710468c4efb8918949f0daae3b23fa39d0b6a483
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-04-20 11:34:45 +0900

    Initial Commit

---
+++ Shared/IntroSectionWithDropdownNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroSectionWithDropdownNavigationApp.swift
+//  Shared
+//
+//  Created by Jinwook Kim on 2022/04/20.
+//
+
+import SwiftUI
+
+@main
+struct IntroSectionWithDropdownNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
